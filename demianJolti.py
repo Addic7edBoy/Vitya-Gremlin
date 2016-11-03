8f2a5fb29c74ae5fd14bd22da9b7d7bc2ae91996
Rootbruh <tratatamaxim@gmail.com>
2016-11-04 03:27:16 +0700

    logo gb update
    new view animation added. The code is more correct now

--- demianJolti.py
+++ demianJolti.py
@@ -17,23 +17,103 @@ if name in access_list:
     p4 = subprocess.Popen(cmd4, shell=True)
     import time
     time.sleep(3)
-    print('000000000000000000000000000000000000000000000000000000000000000000000000000000000000000')
-    print('000000000000000000000000000000000000000000000111111111111100000000000000000000000000000')
-    print('000000000000000000000001111111111111110000001111111111111111000000000000000000000000000')
-    print('000000000000000000000111111111111111110000001110000000000001110000000000000000000000000')
-    print('000000000000000000001111000000000001110000001110000000000001110000000000000000000000000')
-    print('000000000000000000011110000000000001110000001110000000000001110000000000000000000000000')
-    print('000000000000000000011110000000000000000000001110000000000001110000000000000000000000000')
-    print('000000000000000000011110000000000000000000001111111111111111000000000000000000000000000')
-    print('000000000000000000011110000000000000000000001111111111111110000000000000000000000000000')
-    print('000000000000000000011110000011111111110000001110000000000001110000000000000000000000000')
-    print('000000000000000000011110000011111111110000001110000000000001110000000000000000000000000')
-    print('000000000000000000011110000000000001110000001110000000000001110000000000000000000000000')
-    print('000000000000000000001111000000000001110000001110000000000001110000000000000000000000000')
-    print('000000000000000000000111111111111111110000001111111111111111100000000000000000000000000')
-    print('000000000000000000000001111111111111110000001111111111111110000000000000000000000000000')
-    print('000000000000000000000000000000000000000000000000000000000000000000000000000000000000000')
-    print('000000000000000000000000000000000000000000000000000000000000000000000000000000000000000')
+    from time import sleep
+line1 = '0'*90
+line2 = '0'*90
+line3 = '0'*23
+line3_1 = '1'*15
+line3_2 = '0'*6
+line3_3 = '1'*16
+line3_4 = '0'*30
+line4 = '0'*21
+line4_1 = '1'*17
+line4_2 = '0'*6
+line4_3 = '1'*3
+line4_4 = '0'*12
+line4_5 = '1'*3
+line4_6 = '0'*28
+line5 = '0'*20
+line5_1 = '1'*4
+line5_2 = '0'*11
+line5_3 = '1'*3
+line5_4 = '0'*6
+line5_5 = '1'*3
+line5_6 = '0'*12
+line5_7 = '1'*3
+line5_8 = '0'*28
+for i in range(len(line1)):
+    sleep(.01)
+    print (line1[i],end='',flush=True)
+print('0')
+for i in range(len(line2)):
+    sleep(.01)
+    print(line2[i], end='',flush=True)
+print('0')
+for i in range(len(line3)):
+    sleep(.01)
+    print(line3[i], end='',flush=True)
+for i in range(len(line3_1)):
+    print(line3_1[i], end='', flush=True)
+for i in range(len(line3_2)):
+    sleep(.01)
+    print(line3_2[i], end='',flush=True)
+for i in range(len(line3_3)):
+    sleep(.01)
+    print(line3_3[i], end='',flush=True)
+for i in range(len(line3_4)):
+    sleep(.01)
+    print(line3_4[i], end='',flush=True)
+print('0')
+for i in range(len(line4)):
+    sleep(.01)
+    print(line4[i], end='',flush=True)
+for i in range(len(line4_1)):
+    sleep(.01)
+    print(line4_1[i], end='',flush=True)
+for i in range(len(line4_2)):
+    sleep(.01)
+    print(line4_2[i], end='',flush=True)
+for i in range(len(line4_3)):
+    sleep(.01)
+    print(line4_3[i], end='',flush=True)
+for i in range(len(line4_4)):
+    sleep(.01)
+    print(line4_4[i], end='',flush=True)
+for i in range(len(line4_5)):
+    sleep(.01)
+    print(line4_5[i], end='',flush=True)
+for i in range(len(line4_6)):
+    sleep(.01)
+    print(line4_6[i], end='',flush=True)
+print('0')
+for i in range(len(line5)):
+    sleep(.01)
+    print(line5[i], end='',flush=True)
+for i in range(len(line5_1)):
+    sleep(.01)
+    print(line5_1[i], end='',flush=True)
+for i in range(len(line5_2)):
+    sleep(.01)
+    print(line5_2[i], end='',flush=True)
+for i in range(len(line5_3)):
+    sleep(.01)
+    print(line5_3[i], end='',flush=True)
+for i in range(len(line5_4)):
+    sleep(.01)
+    print(line5_4[i], end='',flush=True)
+for i in range(len(line5_5)):
+    sleep(.01)
+    print(line5_5[i], end='',flush=True)
+for i in range(len(line5_6)):
+    sleep(.01)
+    print(line5_6[i], end='',flush=True)
+for i in range(len(line5_7)):
+    sleep(.01)
+    print(line5_7[i], end='',flush=True)
+for i in range(len(line5_8)):
+    sleep(.01)
+    print(line5_8[i], end='',flush=True)
+print('0')
     print('===================================ACCESS GRANTED======================================')
     print('Type "start" to join GBSQUAD')
     x = input()
