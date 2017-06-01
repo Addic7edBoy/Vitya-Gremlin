b8c6456ebeaf9563576e4c5aac1cc02f64b4d56a
Addic7ed <tratatamaxim@gmail.com>
2017-06-01 12:36:01 +0700

    ready to go

---
+++ GBSQUAD.py
@@ -0,0 +1,464 @@
+"""abdul kadirov."""
+
+import subprocess
+import webbrowser
+from time import sleep
+
+
+print("Greetings, stranger. This programm's main goal is to praise GB")
+print("So if you want to see what's inside you have to show us your devotion")
+print('Enter password')
+pas = input()
+pas_list = ['In gb we trust', 'in gb we trust']
+if pas in pas_list:
+    print("Enter user's name")
+    name = input()
+    access_list = ['MAX', 'LEXX', 'GOJA', 'VLAD', 'EGOR']
+    if name in access_list:
+        cmd = 'color 02'
+        cmd2 = 'tree'
+        cmd3 = 'tree'
+        cmd4 = 'tree'
+    
+        PIPE = subprocess.PIPE
+        p = subprocess.Popen(cmd, shell=True)
+        p2 = subprocess.Popen(cmd2, shell=True)
+        p3 = subprocess.Popen(cmd3, shell=True)
+        p4 = subprocess.Popen(cmd4, shell=True)
+        sleep(3)
+        line1 = '0'*118
+        line2 = '0'*118
+        line3 = '0'*40
+        line3_1 = '1'*15
+        line3_2 = '0'*6
+        line3_3 = '1'*16
+        line3_4 = '0'*41
+        line4 = '0'*38
+        line4_1 = '1'*17
+        line4_2 = '0'*6
+        line4_3 = '1'*3
+        line4_4 = '0'*12
+        line4_5 = '1'*3
+        line4_6 = '0'*39
+        line5 = '0'*37
+        line5_1 = '1'*4
+        line5_2 = '0'*11
+        line5_3 = '1'*3
+        line5_4 = '0'*6
+        line5_5 = '1'*3
+        line5_6 = '0'*12
+        line5_7 = '1'*3
+        line5_8 = '0'*39
+        line6 = '0'*36
+        line6_1 = '1'*4
+        line6_2 = '0'*12
+        line6_3 = '1'*3
+        line6_4 = '0'*6
+        line6_5 = '1'*3
+        line6_6 = '0'*12
+        line6_7 = '1'*3
+        line6_8 = '0'*39
+        line7 = '0'*36
+        line7_1 = '1'*4
+        line7_2 = '0'*21
+        line7_3 = '1'*3
+        line7_4 = '0'*12
+        line7_5 = '1'*3
+        line7_6 = '0'*39
+        line8 = '0'*36
+        line8_1 ='1'*4
+        line8_2 = '0'*21
+        line8_3 = '1'*16
+        line8_4 = '0'*41
+        line9 = '0'*36
+        line9_1 ='1'*4
+        line9_2 = '0'*21
+        line9_3 = '1'*15
+        line9_4 = '0'*42
+        line10 = '0'*36
+        line10_1 ='1'*4
+        line10_2 = '0'*5
+        line10_3 = '1'*10
+        line10_4 = '0'*6
+        line10_5 = '1'*3
+        line10_6 = '0'*12
+        line10_7 = '1'*3
+        line10_8 = '0'*39
+        line11 = '0'*36
+        line11_1 ='1'*4
+        line11_2 = '0'*5
+        line11_3 = '1'*10
+        line11_4 = '0'*6
+        line11_5 = '1'*3
+        line11_6 = '0'*12
+        line11_7 = '1'*3
+        line11_8 = '0'*39
+        line12 = '0'*36
+        line12_1 ='1'*4
+        line12_2 = '0'*12
+        line12_3 = '1'*3
+        line12_4 = '0'*6
+        line12_5 = '1' * 3
+        line12_6 = '0' * 12
+        line12_7 = '1' * 3
+        line12_8 = '0' * 39
+        line13 = '0' * 37
+        line13_1 = '1' * 4
+        line13_2 = '0' * 11
+        line13_3 = '1' * 3
+        line13_4 = '0' * 6
+        line13_5 = '1' * 3
+        line13_6 = '0' * 12
+        line13_7 = '1' * 3
+        line13_8 = '0' * 39
+        line14 = '0' * 38
+        line14_1 = '1' * 17
+        line14_2 = '0' * 6
+        line14_3 = '1' * 17
+        line14_4 = '0' * 40
+        line15 = '0' * 40
+        line15_1 = '1' * 15
+        line15_2 = '0' * 6
+        line15_3 = '1' * 15
+        line15_4 = '0' * 42
+        line16 = '0' * 118
+        line17 = '0' * 118
+        for i in range(len(line1)):
+            sleep(.01)
+            print(line1[i], end='', flush=True)
+        print('0')
+        for i in range(len(line2)):
+            sleep(.01)
+            print(line2[i], end='', flush=True)
+        print('0')
+        for i in range(len(line3)):
+            sleep(.01)
+            print(line3[i], end='', flush=True)
+        for i in range(len(line3_1)):
+            sleep(.01)
+            print(line3_1[i], end='', flush=True)
+        for i in range(len(line3_2)):
+            sleep(.01)
+            print(line3_2[i], end='', flush=True)
+        for i in range(len(line3_3)):
+            sleep(.01)
+            print(line3_3[i], end='', flush=True)
+        for i in range(len(line3_4)):
+            sleep(.01)
+            print(line3_4[i], end='', flush=True)
+        print('0')
+        for i in range(len(line4)):
+            sleep(.01)
+            print(line4[i], end='', flush=True)
+        for i in line4_1:
+            sleep(.01)
+            print(i, end='', flush=True)
+        for i in range(len(line4_2)):
+            sleep(.01)
+            print(line4_2[i], end='', flush=True)
+        for i in range(len(line4_3)):
+            sleep(.01)
+            print(line4_3[i], end='', flush=True)
+        for i in range(len(line4_4)):
+            sleep(.01)
+            print(line4_4[i], end='', flush=True)
+        for i in range(len(line4_5)):
+            sleep(.01)
+            print(line4_5[i], end='', flush=True)
+        for i in range(len(line4_6)):
+            sleep(.01)
+            print(line4_6[i], end='', flush=True)
+        print('0')
+        for i in range(len(line5)):
+            sleep(.01)
+            print(line5[i], end='', flush=True)
+        for i in range(len(line5_1)):
+            sleep(.01)
+            print(line5_1[i], end='', flush=True)
+        for i in range(len(line5_2)):
+            sleep(.01)
+            print(line5_2[i], end='', flush=True)
+        for i in range(len(line5_3)):
+            sleep(.01)
+            print(line5_3[i], end='',flush=True)
+        for i in range(len(line5_4)):
+            sleep(.01)
+            print(line5_4[i], end='',flush=True)
+        for i in range(len(line5_5)):
+            sleep(.01)
+            print(line5_5[i], end='',flush=True)
+        for i in range(len(line5_6)):
+            sleep(.01)
+            print(line5_6[i], end='',flush=True)
+        for i in range(len(line5_7)):
+            sleep(.01)
+            print(line5_7[i], end='',flush=True)
+        for i in range(len(line5_8)):
+            sleep(.01)
+            print(line5_8[i], end='',flush=True)
+        print('0')
+        for i in range(len(line6)):
+            sleep(.01)
+            print (line6[i],end='',flush=True)
+        for i in range(len(line6_1)):
+            sleep(.01)
+            print (line6_1[i],end='',flush=True)
+        for i in range(len(line6_2)):
+            sleep(.01)
+            print (line6_2[i],end='',flush=True)
+        for i in range(len(line6_3)):
+            sleep(.01)
+            print (line6_3[i],end='',flush=True)
+        for i in range(len(line6_4)):
+            sleep(.01)
+            print (line6_4[i],end='',flush=True)
+        for i in range(len(line6_5)):
+            sleep(.01)
+            print (line6_5[i],end='',flush=True)
+        for i in range(len(line6_6)):
+            sleep(.01)
+            print (line6_6[i],end='',flush=True)
+        for i in range(len(line6_7)):
+            sleep(.01)
+            print (line6_7[i],end='',flush=True)
+        for i in range(len(line6_8)):
+            sleep(.01)
+            print (line6_8[i],end='',flush=True)
+        print('0')
+        for i in range(len(line7)):
+            sleep(.01)
+            print (line7[i],end='',flush=True)
+        for i in range(len(line7_1)):
+            sleep(.01)
+            print (line7_1[i],end='',flush=True)
+        for i in range(len(line7_2)):
+            sleep(.01)
+            print (line7_2[i],end='',flush=True)
+        for i in range(len(line7_3)):
+            sleep(.01)
+            print (line7_3[i],end='',flush=True)
+        for i in range(len(line7_4)):
+            sleep(.01)
+            print (line7_4[i],end='',flush=True)
+        for i in range(len(line7_5)):
+            sleep(.01)
+            print (line7_5[i],end='',flush=True)
+        for i in range(len(line7_6)):
+            sleep(.01)
+            print (line7_6[i],end='',flush=True)
+        print('0')
+        for i in range(len(line8)):
+            sleep(.01)
+            print(line8[i], end='',flush=True)
+        for i in range(len(line8_1)):
+            sleep(.01)
+            print(line8_1[i], end='',flush=True)
+        for i in range(len(line8_2)):
+            sleep(.01)
+            print(line8_2[i], end='',flush=True)
+        for i in range(len(line8_3)):
+            sleep(.01)
+            print(line8_3[i], end='',flush=True)
+        for i in range(len(line8_4)):
+            sleep(.01)
+            print(line8_4[i], end='',flush=True)
+        print('0')
+        for i in range(len(line9)):
+            sleep(.01)
+            print(line9[i], end='',flush=True)
+        for i in range(len(line9_1)):
+            sleep(.01)
+            print(line9_1[i], end='',flush=True)
+        for i in range(len(line9_2)):
+            sleep(.01)
+            print(line9_2[i], end='',flush=True)
+        for i in range(len(line9_3)):
+            sleep(.01)
+            print(line9_3[i], end='',flush=True)
+        for i in range(len(line9_4)):
+            sleep(.01)
+            print(line9_4[i], end='',flush=True)
+        print('0')
+        for i in range(len(line10)):
+            sleep(.01)
+            print(line10[i], end='',flush=True)
+        for i in range(len(line10_1)):
+            sleep(.01)
+            print(line10_1[i], end='',flush=True)
+        for i in range(len(line10_2)):
+            sleep(.01)
+            print(line10_2[i], end='',flush=True)
+        for i in range(len(line10_3)):
+            sleep(.01)
+            print(line10_3[i], end='',flush=True)
+        for i in range(len(line10_4)):
+            sleep(.01)
+            print(line10_4[i], end='',flush=True)
+        for i in range(len(line10_5)):
+            sleep(.01)
+            print(line10_5[i], end='',flush=True)
+        for i in range(len(line10_6)):
+            sleep(.01)
+            print(line10_6[i], end='',flush=True)
+        for i in range(len(line10_7)):
+            sleep(.01)
+            print(line10_7[i], end='',flush=True)
+        for i in range(len(line10_8)):
+            sleep(.01)
+            print(line10_8[i], end='',flush=True)
+        print('0')
+        for i in range(len(line11)):
+            sleep(.01)
+            print(line11[i], end='',flush=True)
+        for i in range(len(line11_1)):
+            sleep(.01)
+            print(line11_1[i], end='',flush=True)
+        for i in range(len(line11_2)):
+            sleep(.01)
+            print(line11_2[i], end='',flush=True)
+        for i in range(len(line11_3)):
+            sleep(.01)
+            print(line11_3[i], end='',flush=True)
+        for i in range(len(line11_4)):
+            sleep(.01)
+            print(line11_4[i], end='',flush=True)
+        for i in range(len(line11_5)):
+            sleep(.01)
+            print(line11_5[i], end='',flush=True)
+        for i in range(len(line11_6)):
+            sleep(.01)
+            print(line11_6[i], end='',flush=True)
+        for i in range(len(line11_7)):
+            sleep(.01)
+            print(line11_7[i], end='',flush=True)
+        for i in range(len(line11_8)):
+            sleep(.01)
+            print(line11_8[i], end='',flush=True)
+        print('0')
+        for i in range(len(line12)):
+            sleep(.01)
+            print(line12[i], end='',flush=True)
+        for i in range(len(line12_1)):
+            sleep(.01)
+            print(line12_1[i], end='',flush=True)
+        for i in range(len(line12_2)):
+            sleep(.01)
+            print(line12_2[i], end='',flush=True)
+        for i in range(len(line12_3)):
+            sleep(.01)
+            print(line12_3[i], end='',flush=True)
+        for i in range(len(line12_4)):
+            sleep(.01)
+            print(line12_4[i], end='',flush=True)
+        for i in range(len(line12_5)):
+            sleep(.01)
+            print(line12_5[i], end='',flush=True)
+        for i in range(len(line12_6)):
+            sleep(.01)
+            print(line12_6[i], end='',flush=True)
+        for i in range(len(line12_7)):
+            sleep(.01)
+            print(line12_7[i], end='',flush=True)
+        for i in range(len(line12_8)):
+            sleep(.01)
+            print(line12_8[i], end='',flush=True)
+        print('0')
+        for i in range(len(line13)):
+            sleep(.01)
+            print(line13[i], end='',flush=True)
+        for i in range(len(line13_1)):
+            sleep(.01)
+            print(line13_1[i], end='',flush=True)
+        for i in range(len(line13_2)):
+            sleep(.01)
+            print(line13_2[i], end='',flush=True)
+        for i in range(len(line13_3)):
+            sleep(.01)
+            print(line13_3[i], end='',flush=True)
+        for i in range(len(line13_4)):
+            sleep(.01)
+            print(line13_4[i], end='',flush=True)
+        for i in range(len(line13_5)):
+            sleep(.01)
+            print(line13_5[i], end='',flush=True)
+        for i in range(len(line13_6)):
+            sleep(.01)
+            print(line13_6[i], end='',flush=True)
+        for i in range(len(line13_7)):
+            sleep(.01)
+            print(line13_7[i], end='',flush=True)
+        for i in range(len(line13_8)):
+            sleep(.01)
+            print(line13_8[i], end='',flush=True)
+        print('0')
+        for i in range(len(line14)):
+            sleep(.01)
+            print(line14[i], end='',flush=True)
+        for i in range(len(line14_1)):
+            sleep(.01)
+            print(line14_1[i], end='',flush=True)
+        for i in range(len(line14_2)):
+            sleep(.01)
+            print(line14_2[i], end='',flush=True)
+        for i in range(len(line14_3)):
+            sleep(.01)
+            print(line14_3[i], end='',flush=True)
+        for i in range(len(line14_4)):
+            sleep(.01)
+            print(line14_4[i], end='',flush=True)
+        print('0')
+        for i in range(len(line15)):
+            sleep(.01)
+            print(line15[i], end='',flush=True)
+        for i in range(len(line15_1)):
+            sleep(.01)
+            print(line15_1[i], end='',flush=True)
+        for i in range(len(line15_2)):
+            sleep(.01)
+            print(line15_2[i], end='',flush=True)
+        for i in range(len(line15_3)):
+            sleep(.01)
+            print(line15_3[i], end='',flush=True)
+        for i in range(len(line15_4)):
+            sleep(.01)
+            print(line15_4[i], end='',flush=True)
+        print('0')
+        for i in range(len(line16)):
+            sleep(.01)
+            print (line16[i],end='',flush=True)
+        print('0')
+        for i in range(len(line17)):
+            sleep(.01)
+            print(line17[i], end='',flush=True)
+        print('0')
+        print('='*52,'ACCESS GRANTED', '='*52)
+        print('Type "start" to join GBSQUAD')
+        x = input()
+        if x == ('start'):
+            webbrowser.open('https://vk.com/club109913725')
+    else:
+        cmd5 = 'color 04'
+    
+        PIPE = subprocess.PIPE
+        p5 = subprocess.Popen(cmd5, shell=True)
+        inf = 'ACCESS DENIED '*404
+        for i in range(len(inf)):
+            sleep(.06)
+            print(inf[i], end='', flush=True)
+        cmd6 = 'shutdown /r'
+    
+        PIPE = subprocess.PIPE
+        p6 = subprocess.Popen(cmd6, shell=True)
+else:
+    cmd5 = 'color 04'
+    
+    PIPE = subprocess.PIPE
+    p5 = subprocess.Popen(cmd5, shell=True)
+    inf = 'ACCESS DENIED '*404
+    for i in range(len(inf)):
+        sleep(.06)
+        print(inf[i], end='', flush=True)
+        cmd6 = 'shutdown /r'
+    
+    PIPE = subprocess.PIPE
+    p6 = subprocess.Popen(cmd6, shell=True)
